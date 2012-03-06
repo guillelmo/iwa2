255616686ff9aa106aa79c9c5e6650301d941ca1
Guillermo S <guillelmo@gmail.com>
2012-03-06 02:48:33 +0100

    Updated Kasper modifications 3/3/12

---
+++ iwa-as2.1.py
@@ -0,0 +1,36 @@
+import cgi
+import datetime
+import wsgiref.handlers
+import os
+
+from google.appengine.ext.webapp import template
+from google.appengine.ext import db
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.api import urlfetch
+
+
+    
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        url = "http://api.eventful.com/rest/events/search?app_key=fZbWS6qHrqnNvPW7&keywords=bombay+show+pig"
+
+        template_values = {
+        }
+
+        result = urlfetch.fetch(url)
+
+        if result.status_code == 200:
+            self.response.out.write(result.content)
+        else:
+            path = os.path.join(os.path.dirname(__file__), "index2.html")
+            self.response.out.write(template.render(path, template_values))
+
+application = webapp.WSGIApplication([('/', MainPage)], debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
--- iwa-as2.py
+++ iwa-as2.py
@@ -2,21 +2,50 @@ import cgi
 import datetime
 import wsgiref.handlers
 import os
+import re
 
 from google.appengine.ext.webapp import template
 from google.appengine.ext import db
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.api import urlfetch
+
     
 class MainPage(webapp.RequestHandler):
     def get(self):
         template_values = {
         }
 
-        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        path = os.path.join(os.path.dirname(__file__), "index.html")
         self.response.out.write(template.render(path, template_values))
-    
+
+    def post(self):
+        query = self.request.get("content")
+
+        literals = re.findall(r'"(.+?)"',query)
+
+        urls = processLiterals(literals)
+
+        for url in urls:
+            xml = urlfetch.fetch(url)
+            if xml.status_code == 200:
+                print "XML retrieved."
+            else:
+                print "Something went wrong with the connection to the Eventful server. Status code: " + xml.status_code
+
+def processLiterals(list):
+    baseurl = "http://api.eventful.com/rest/events/search?app_key=fZbWS6qHrqnNvPW7&keywords="
+
+    urls = []
+    i=0
+    for item in list:
+        item = item.replace(" ", "+")
+        url = baseurl + item
+        urls.insert(i,url)
+        i=i+1
+
+    return urls
 
 application = webapp.WSGIApplication([('/', MainPage)], debug=True)
 
